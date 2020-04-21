f7bf4c1e2c436c765533d03fe86de00aa4f8b186
Akash <tech@bleetech.in>
2020-04-21 14:36:07 +0530

    Code Optimization -> Models Structure Changed

--- app/src/main/java/com/vinu/vinodassigment/models/NewsModel.kt
+++ app/src/main/java/com/vinu/vinodassigment/models/NewsModel.kt
@@ -1,12 +1,13 @@
 package com.vinu.vinodassigment.models
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
-
 
 @Parcelize
 data class NewsModel(
-    var title: String? = null,
-    var description: String? = null,
-    var imageHref: String? = null
+    @PrimaryKey @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "description") var description: String?,
+    @ColumnInfo(name = "imageHref") var imageHref: String?
 ) : Parcelable
--- app/src/main/java/com/vinu/vinodassigment/models/ResponseModel.kt
+++ app/src/main/java/com/vinu/vinodassigment/models/ResponseModel.kt
@@ -1,20 +1,16 @@
 package com.vinu.vinodassigment.models
 
 import android.os.Parcelable
-import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @Entity(tableName = "news_data")
 data class ResponseModel(
-    var title: String = "",
-    var rows: List<NewsModel>? = ArrayList()
-) : Parcelable {
-    @IgnoredOnParcel
-    @PrimaryKey(autoGenerate = true)
-    @NonNull
-    var id: Int = 1
-}
+    @PrimaryKey @ColumnInfo(name = "title") var title: String = "",
+    var rows: List<NewsModel>? = ArrayList(),
+    var exceptionMsg: String = "",
+    var isDataFromDb: Boolean = false
+) : Parcelable
